dd1a505ecbfb722f79f4acb40513721ea9561f77
Sumon Debnath <debnathsumon1708@gmail.com>
2025-10-22 20:05:51 +0600

    Packages and functionlities add html module escaped unescaped functions

--- Packages and Functionalitites/HTML_module.py
+++ Packages and Functionalitites/HTML_module.py
@@ -1,3 +1,11 @@
 import html
 
 print(html.unescape('&pound;682m'))
+
+text = 'This is <b>bold</b> & "quoted" text.'
+escaped = html.escape(text)
+print(escaped)
+
+escaped = 'This is &lt;b&gt;bold&lt;/b&gt; &amp; &quot;quoted&quot; text.'
+unescaped = html.unescape(escaped)
+print(unescaped)
